e16c293a6932012ad4675a3ef9f940817d0b7980
Sunimt <dorysunim@gmail.com>
2022-11-08 12:39:28 -0700

    first commit

---
+++ libreria2_DSMT/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace libreria2_DSMT
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
